e6e2fa45bcbefd3d8de028b1366507b65fdfbb84
José Carlos Pereira Vieira <josecarlosv8474@gmail.com>
2021-07-27 11:29:55 -0300

    Modificado a pasta Projetos/Projeto03 com os arquivos

--- Projetos/Projeto03/script.js
+++ Projetos/Projeto03/script.js
@@ -3,20 +3,23 @@ function verificarIMC(){
 var peso = document.getElementById("peso").value
 var altura = document.getElementById("altura").value
 
-var imc = parseInt(peso)/(parseFloat(altura)*parseFloat(altura))
+var imcQuebrado = parseInt(peso)/(parseFloat(altura)*parseFloat(altura))
 
-if(imc<17){
+var imcFinal = imcQuebrado.toFixed(2)
 
-     alert (imc+" Você está Muito abaixo do peso")
-}else if(imc>17&&imc<18.49){
-     alert (imc+" Você está abaixo do peso")
-}else if(imc>18.5&&imc<24.99){
-    alert (imc+ " Parabéns, você está no peso ideal")
-}else if(imc>25&&imc<29.99){
-    alert (imc+ " Você está acima do peso")
-}else if(imc>30&&imc<34.99){
-    alert (imc+ " Você está obeso")
-}else if(imc>35){
-    alert (imc+ " Cuidado! Você está com obesidade mórbida")
+
+if(imcFinal<17){
+
+    document.getElementById("res").innerHTML= imcFinal+": Você está muito abaixo do peso"
+}else if(imcFinal>17&&imcFinal<18.49){
+    document.getElementById("res").innerHTML = imcFinal+": Você está abaixo do peso"
+}else if(imcFinal>18.5&&imcFinal<24.99){
+    document.getElementById("res").innerHTML = imcFinal+ ": Parabéns, você está no peso ideal"
+}else if(imcFinal>25&&imcFinal<29.99){
+    document.getElementById("res").innerHTML = imcFinal+ ": Você está acima do peso"
+}else if(imcFinal>30&&imcFinal<34.99){
+    document.getElementById("res").innerHTML = imcFinal+ ": Você está obeso"
+}else if(imcFinal>35){
+    document.getElementById("res").innerHTML = imcFinal+ ": Cuidado! Você está com obesidade mórbida"
 }
 }
